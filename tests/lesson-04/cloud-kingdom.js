8eedfe5915fbdfcdb0f85f1f8e23d07c82c09d74
test 01 - vuitran <tranthivui274@gmail.com>
2026-07-29 21:49:01 +0700

    feat: add solution for challenge 03

--- tests/lesson-04/cloud-kingdom.js
+++ tests/lesson-04/cloud-kingdom.js
@@ -1,15 +1,15 @@
 let powerUp='mushroom';
 let effect='';
-if (powerUp=='mushroom'){
+if (powerUp==='mushroom'){
     effect='Mario becomes Super!';
 }else{
-    if(powerUp=='flower'){
+    if(powerUp==='flower'){
         effect='Mario can shoot fireballs';
     }else{
-        if(powerUp=='star'){
+        if(powerUp==='star'){
             effect='Mario is invincible!';
         }else{
-            if(powerUp=='none'){
+            if(powerUp==='none'){
                 effect='Mario is normal';
             }else{
                 effect='Unknown power-up'
